b44f81855f7154a913af08553095f81f02944a9d
Eduardo Medel <127570392+medelzin@users.noreply.github.com>
2024-11-01 15:40:32 -0300

    update trabalho.sh

--- trabalho.sh
+++ trabalho.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+#1
+cd ~
+
+mkdir joao
+mkdir maria
+
+#2
+cd joao
+cp -r /etc/systemd .
+
+#3
+cd ~
+cd maria
+cp -r /etc/passwd .
+cp -r /etc/group .
+
+#4
+cd ~
+cp -r joao joao_copia
+cp -r maria maria_copia
+
+#5
+mkdir mais_uma_pasta
+cd mais_uma_pasta
+cp -r /home/aluno/joao .
+cp -r /home/aluno/maria .
+
+#6
+mv maria maria2
+mv joao maria 
+mv maria2 joao
+
+
+
 # NAO APAGUE A LINHA ACIMA!
 
 
